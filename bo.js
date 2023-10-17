07d30810ed03d1b225e5d0ae21d32cca86c6e92e
Vijf7eendrie <144676959+Vijf7eendrie@users.noreply.github.com>
2023-10-17 15:14:30 +0200

    Bo.js

--- bo.js
+++ bo.js
@@ -22,7 +22,7 @@ function beginverhaal() {
           vn = 1;
           hervatverhaal();
         } else if (ant === "nee") {
-          console.log("Nou dan niet!");
+          console.log("Helaas ,Nou dan niet!");
           rl.close();
         }
       });
@@ -41,39 +41,10 @@ function hervatverhaal() {
         vn = 2;
       } else if (ant === "Real Madrid") {
         vn = 3;
-        let validAnswer = "Feyenoord";
-        while (!validAnswer) 
+        hervatverhaal();
         console.log("Tijdens je aankondiging lachte mensen je uit de club heeft je club per direct geterminate!.");
         console.log("Het verhaal eindigt hier.");
         rl.close();
       }
     });
-  } else if (vn === 2) {
-    console.log("Alex betreedt de grot en ontdekt een doolhof van gangen en kamers.");
-    console.log("Hij moet de juiste keuzes maken om de schat te vinden.");
-    console.log("Welke kant wil je op?");
-    console.log("'links'");
-    console.log("'rechts'");
-    rl.question("Antwoord: ", (ant) => {
-      if (ant === "links") {
-        vn = 4;
-        continueStory();
-      } else if (ant === "rechts") {
-        vn = 5;
-        console.log("Alex gaat rechts en komt vast te zitten in een val.");
-        console.log("Het verhaal eindigt hier.");
-        rl.close();
-      }
-    });
-  } else if (vn === 4) {
-    console.log("Alex gaat links en vindt een glinsterende schatkist.");
-    console.log("Hij opent de kist en vindt een enorme berg goud en juwelen.");
-    console.log("Gefeliciteerd, Alex heeft de legendarische schat gevonden!");
-    rl.close();
-  } else {
-    console.log("Dit is het einde van het verhaal. Bedankt voor het spelen!");
-    rl.close();
-  }
-}
 
-beginverhaal();
